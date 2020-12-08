8e94f84ca75762dfebb2b27c0dcac8b99924ffb7
sektor123 <stratonov1234@gmail.com>
2020-12-08 20:00:08 +0200

    fixed jump and added simple attack logic

--- Assets/Scripts/Game/ActionController.cs
+++ Assets/Scripts/Game/ActionController.cs
@@ -1,13 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Game
 {
     public class ActionController : MonoBehaviour
     {
+        private enum _Direction
+        {
+            Left = -1,
+            Right = 1
+        }
+        
         [SerializeField] private float _movementSpeed;
         [SerializeField] private float _jumpForceMultiplier;
+        [SerializeField] private GameObject _attackSprite;
+        private bool _bCanJump = true;
+        private bool _bCanAttack = true;
+        private Vector3 _jumpDirection = new Vector3(0.0f, 1.0f, 0.0f);
+        private float lastAttackTime = -0.2f;
+        
         
         private Rigidbody2D _rb;
+
+        private void changeDirection(_Direction direction)
+        {
+            var currentScale = transform.localScale;
+            currentScale.x = Mathf.Abs(currentScale.x) * (int)direction;
+            transform.localScale = currentScale;
+        }
         
         private void Awake()
         {
@@ -16,24 +36,31 @@ namespace Game
 
         public void ActionMoveLeft()
         {
+            changeDirection(_Direction.Left);
             var movePosition = new Vector3(-1 * _movementSpeed * Time.fixedDeltaTime, 0, 0);
             _rb.AddForce(movePosition);
         }
 
         public void ActionMoveRight()
         {
+            changeDirection(_Direction.Right);
             var movePosition = new Vector3(_movementSpeed * Time.fixedDeltaTime, 0, 0);
             _rb.AddForce(movePosition);
         }
 
         public void ActionAttack()
         {
-            
+            if (!_bCanAttack) return;
+            _bCanAttack = false;
+            _attackSprite.SetActive(true);
+            lastAttackTime = Time.time;
         }
 
         public void ActionJump()
         {
-            var jumpForce = new Vector3(0, 1 * Time.fixedDeltaTime * _jumpForceMultiplier, 0);
+            if (!_bCanJump) return;
+            _bCanJump = false;
+            var jumpForce = _jumpDirection * (Time.fixedDeltaTime * _jumpForceMultiplier);// new Vector3(0, 1 * Time.fixedDeltaTime * _jumpForceMultiplier, 0);
             _rb.AddForce(jumpForce, ForceMode2D.Impulse);
         }
 
@@ -48,11 +75,40 @@ namespace Game
             {
                 ActionMoveRight();
             }
-            
+        }
+
+        private void Update()
+        {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 ActionJump();    
             }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                ActionAttack();
+            }
+
+            if (Time.time - lastAttackTime > 0.2f)
+            {
+                _bCanAttack = true;
+                _attackSprite.SetActive(false);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            _bCanJump = true;
+            var _normal = new Vector2(0.0f, 4.0f);
+            
+            foreach (var contactPoint in other.contacts)
+            {
+                _normal = (_normal + contactPoint.normal) / 2.0f;
+            }
+            _jumpDirection = new Vector3(_normal.x, _normal.y, 0.0f).normalized;
+            
+            Debug.Log(_jumpDirection);
         }
     }
+    
 }
